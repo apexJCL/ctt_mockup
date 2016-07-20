99484a7509df8e3f08c2887af068172aa00ec9a8
José Carlos <12030800@itcelaya.edu.mx>
2016-07-20 01:34:31 -0500

    v0.1

--- views/showcase/index.php
+++ views/showcase/index.php
@@ -2,25 +2,29 @@
 /* @var $this yii\web\View */
 ?>
 <div class="parallax-container">
-    <div class="parallax"><img src="\img\showcase\lenses.jpg" alt=""></div>
+    <div class="parallax"><img src="\img\showcase.jpg" alt=""></div>
 </div>
 
 <div class="section grey lighten-4 scrollspy" id="clients">
     <div class="container">
-        <h1 class="roboto-thin">
+        <h1 class="raleway-bold">
+            <div style="background-color:  rgb(135, 74, 233); max-width: 50px; min-height: 15px;"></div>
             Cámaras
         </h1>
     </div>
-    <div class="row">
-        <div class="col s12 center">
-            <img src="\img\showcase\lenses.jpg" alt="">
-        </div>
+    <div class="row container">
         <div class="col s12">
-            <p class="flow-text container" style="text-align: justify; justify-content: flex-start">
+            <p class="raleway" style="text-align: justify; justify-content: flex-start">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam maximus ex ut elementum auctor. Pellentesque facilisis quam magna, a blandit dolor aliquam at. Curabitur vitae mollis odio, a facilisis turpis. Aliquam euismod lectus quis lectus placerat pellentesque. Proin et odio ornare sapien ultricies commodo ac quis justo. Pellentesque odio augue, dapibus elementum rutrum ut, suscipit in nunc. Duis libero quam, viverra ut elit eu, feugiat convallis ipsum.
 
                 Nunc augue lectus, euismod bibendum ultricies eget, malesuada id mi. Vestibulum at est ultrices, vulputate ex at, lacinia enim. Proin ut tristique dui. Fusce tincidunt aliquam enim, vel ultricies justo. Mauris condimentum orci at fermentum pharetra. Fusce pellentesque accumsan lectus. Cras semper, purus vitae iaculis egestas, eros nisl suscipit leo, ac pharetra urna leo quis tellus. Aenean eleifend, diam et laoreet consequat, ligula neque fermentum purus, non efficitur magna lectus id odio. Ut tellus est, accumsan ac risus id, malesuada ultricies magna.
             </p>
+        </div>
+        <div class="col s12 center">
+            <img src="\img\showcase_full.jpg" alt="" class="responsive-img">
+        </div>
+        <div class="col s12">
+            <a href="/" class="btn waves-effect blue">Regresar</a>
         </div>
     </div>
 </div>
--- views/site/index.php
+++ views/site/index.php
@@ -9,11 +9,25 @@ $this->title = 'Panel de Control';
 <!-- Welcome -->
 <div class="video-header--container scrollspy" id="home">
     <div class="login">
-        <div class="row home-logo">
-            <div class="col s12 m6 offset-m3 l4 offset-l2 center">
+<!--        <div class="row">-->
+<!--            <div class="col l1 m1 s1 teal">1</div>-->
+<!--            <div class="col l1 m1 s1 teal">2</div>-->
+<!--            <div class="col l1 m1 s1 teal">3</div>-->
+<!--            <div class="col l1 m1 s1 teal">4</div>-->
+<!--            <div class="col l1 m1 s1 teal">5</div>-->
+<!--            <div class="col l1 m1 s1 teal">6</div>-->
+<!--            <div class="col l1 m1 s1 teal">7</div>-->
+<!--            <div class="col l1 m1 s1 teal">8</div>-->
+<!--            <div class="col l1 m1 s1 teal">9</div>-->
+<!--            <div class="col l1 m1 s1 teal">10</div>-->
+<!--            <div class="col l1 m1 s1 teal">11</div>-->
+<!--            <div class="col l1 m1 s1 teal">12</div>-->
+<!--        </div>-->
+        <div class="row margin-big">
+            <div class="col s12 m6 offset-m3 l4 offset-l1 center">
                 <img src="/img/logo.png" alt="" class="responsive-img">
             </div>
-            <div class="col s8 offset-s2 l5">
+            <div class="col s8 offset-s2 l5 title-margin">
                 <span><hr></span>
                 <div class="raleway-bold white-text" style="font-size: 6rem">Wapp Beta</div>
                 <h5 class="raleway-bold white-text">
@@ -22,9 +36,9 @@ $this->title = 'Panel de Control';
             </div>
         </div>
     </div>
-    <video autoplay poster="" id="bgvid" loop>
-        <source src="/video/CDMX.webm" type="video/webm">
-    </video>
+        <video autoplay poster="" id="bgvid" loop>
+            <source src="/video/CDMX.webm" type="video/webm">
+        </video>
 </div>
 <!-- Inventarios -->
 <div class="section-big grey darken-3 white-text scrollspy" id="inventory">
@@ -89,60 +103,83 @@ $this->title = 'Panel de Control';
     </div>
 </div>
 
-<div class="showcase">
-    <div class="row no-padding">
-        <div class="col s12 m6 l4 no-padding caption-container">
-            <img src="/img/showcase/1.jpg" alt="">
+
+<div class="row no-margin">
+    <div class="col s12 m6 l4 no-padding">
+        <div class="caption-container">
+            <img src="/img/showcase/1.jpg" alt="" class="responsive-img">
             <div class="caption-container--container primary-overlay">
-                <div class="caption-container--text">
-                    <h4 class="white-text raleway-bold">Cámara</h4>
-                    <p class="white-text raleway">Equipos de alto desempeño</p>
-                </div>
+                <a href="/showcase/index">
+                    <div class="caption-container--text">
+                        <h4 class="white-text raleway-bold">Cámara</h4>
+                        <p class="white-text raleway">Equipos de alto desempeño</p>
+                    </div>
+                </a>
             </div>
         </div>
-        <div class="col s12 m6 l4 no-padding caption-container">
-            <img src="/img/showcase/2.jpg" alt="">
+    </div>
+    <div class="col s12 m6 l4 no-padding">
+        <div class="caption-container">
+            <img src="/img/showcase/2.jpg" alt="" class="responsive-img">
             <div class="caption-container--container primary-overlay">
-                <div class="caption-container--text">
-                    <h4 class="white-text raleway-bold">Lentes</h4>
-                    <p class="white-text raleway">Las marcas de mayor prestigio</p>
-                </div>
+                <a href="/showcase/index">
+                    <div class="caption-container--text">
+                        <h4 class="white-text raleway-bold">Lentes</h4>
+                        <p class="white-text raleway">Las marcas de mayor prestigio</p>
+                    </div>
+                </a>
             </div>
         </div>
-        <div class="col s12 m6 l4 no-padding caption-container">
-            <img src="/img/showcase/3.jpg" alt="">
+    </div>
+    <div class="col s12 m6 l4 no-padding">
+        <div class="caption-container">
+            <img src="/img/showcase/3.jpg" alt="" class="responsive-img">
             <div class="caption-container--container primary-overlay">
-                <div class="caption-container--text">
-                    <h4 class="white-text raleway-bold">Cabezas</h4>
-                    <p class="white-text raleway">Ámplia variedad de cabezas y sistemas rieles</p>
-                </div>
+                <a href="/showcase/index">
+                    <div class="caption-container--text">
+                        <h4 class="white-text raleway-bold">Cabezas</h4>
+                        <p class="white-text raleway">Ámplia variedad de cabezas y sistemas rieles</p>
+                    </div>
+                </a>
             </div>
         </div>
-        <div class="col s12 m6 l4 no-padding caption-container">
+    </div>
+    <div class="col s12 m6 l4 no-padding">
+        <div class="caption-container">
             <img src="/img/showcase/4.jpg" alt="">
             <div class="caption-container--container primary-overlay">
-                <div class="caption-container--text">
-                    <h4 class="white-text raleway-bold">ARRI Lens</h4>
-                    <p class="white-text raleway">Principal distribuidor de ARRI en México</p>
-                </div>
+                <a href="/showcase/index">
+                    <div class="caption-container--text">
+                        <h4 class="white-text raleway-bold">ARRI Lens</h4>
+                        <p class="white-text raleway">Principal distribuidor de ARRI en México</p>
+                    </div>
+                </a>
             </div>
         </div>
-        <div class="col s12 m6 l4 no-padding caption-container">
+    </div>
+    <div class="col s12 m6 l4 no-padding">
+        <div class="caption-container">
             <img src="/img/showcase/5.jpg" alt="">
             <div class="caption-container--container primary-overlay">
-                <div class="caption-container--text">
-                    <h4 class="white-text raleway-bold">Electrónicos</h4>
-                    <p class="white-text raleway">Lo más sofisticado y avanzado en equipos</p>
-                </div>
+                <a href="/showcase/index">
+                    <div class="caption-container--text">
+                        <h4 class="white-text raleway-bold">Electrónicos</h4>
+                        <p class="white-text raleway">Lo más sofisticado y avanzado en equipos</p>
+                    </div>
+                </a>
             </div>
         </div>
-        <div class="col s12 m6 l4 no-padding caption-container">
+    </div>
+    <div class="col s12 m6 l4 no-padding">
+        <div class="caption-container">
             <img src="/img/showcase/6.jpg" alt="">
             <div class="caption-container--container primary-overlay">
-                <div class="caption-container--text">
-                    <h4 class="white-text raleway-bold">Grúas</h4>
-                    <p class="white-text raleway">Ámplio soporte de grúas Eléctricas</p>
-                </div>
+                <a href="/showcase/index">
+                    <div class="caption-container--text">
+                        <h4 class="white-text raleway-bold">Grúas</h4>
+                        <p class="white-text raleway">Ámplio soporte de grúas Eléctricas</p>
+                    </div>
+                </a>
             </div>
         </div>
     </div>
@@ -165,14 +202,14 @@ $this->title = 'Panel de Control';
     </div>
     <div class="container">
         <div class="row center">
-            <div class="col s12 m6 l3"><img src="http://placehold.it/350x250" alt="" class="responsive-img"></div>
-            <div class="col s12 m6 l3"><img src="http://placehold.it/350x250" alt="" class="responsive-img"></div>
-            <div class="col s12 m6 l3"><img src="http://placehold.it/350x250" alt="" class="responsive-img"></div>
-            <div class="col s12 m6 l3"><img src="http://placehold.it/350x250" alt="" class="responsive-img"></div>
-            <div class="col s12 m6 l3"><img src="http://placehold.it/350x250" alt="" class="responsive-img"></div>
-            <div class="col s12 m6 l3"><img src="http://placehold.it/350x250" alt="" class="responsive-img"></div>
-            <div class="col s12 m6 l3"><img src="http://placehold.it/350x250" alt="" class="responsive-img"></div>
-            <div class="col s12 m6 l3"><img src="http://placehold.it/350x250" alt="" class="responsive-img"></div>
+            <div class="col s12 m4 l3"><img src="/img/brands/brand_1.jpg" alt="" class="responsive-img"></div>
+            <div class="col s12 m4 l3"><img src="/img/brands/brand_2.jpg" alt="" class="responsive-img"></div>
+            <div class="col s12 m4 l3"><img src="/img/brands/brand_3.jpg" alt="" class="responsive-img"></div>
+            <div class="col s12 m4 l3"><img src="/img/brands/brand_4.jpg" alt="" class="responsive-img"></div>
+            <div class="col s12 m4 l3"><img src="/img/brands/brand_5.jpg" alt="" class="responsive-img"></div>
+            <div class="col s12 m4 l3"><img src="/img/brands/brand_6.jpg" alt="" class="responsive-img"></div>
+            <div class="col s12 m4 l3"><img src="/img/brands/brand_7.jpg" alt="" class="responsive-img"></div>
+            <div class="col s12 m4 l3"><img src="/img/brands/brand_8.jpg" alt="" class="responsive-img"></div>
         </div>
     </div>
 </div>
@@ -194,9 +231,9 @@ $this->title = 'Panel de Control';
         </div>
     </div>
 </div>
-<div class="showcase no-padding">
-    <div class="row no-padding">
-        <div class="col s12 m6 l3 no-padding caption-container">
+<div class="row center no-margin">
+    <div class="col s12 m6 l3 no-padding">
+        <div class="caption-container">
             <img src="/img/users/1.jpg" alt="">
             <div class="caption-container--container grey-overlay">
                 <div class="caption-container--text">
@@ -205,7 +242,9 @@ $this->title = 'Panel de Control';
                 </div>
             </div>
         </div>
-        <div class="col s12 m6 l3 no-padding caption-container">
+    </div>
+    <div class="col s12 m6 l3 no-padding">
+        <div class="caption-container">
             <img src="/img/users/2.jpg" alt="">
             <div class="caption-container--container grey-overlay">
                 <div class="caption-container--text">
@@ -214,7 +253,9 @@ $this->title = 'Panel de Control';
                 </div>
             </div>
         </div>
-        <div class="col s12 m6 l3 no-padding caption-container">
+    </div>
+    <div class="col s12 m6 l3 no-padding">
+        <div class="caption-container">
             <img src="/img/users/3.jpg" alt="">
             <div class="caption-container--container grey-overlay">
                 <div class="caption-container--text">
@@ -223,7 +264,9 @@ $this->title = 'Panel de Control';
                 </div>
             </div>
         </div>
-        <div class="col s12 m6 l3 no-padding caption-container">
+    </div>
+    <div class="col s12 m6 l3 no-padding">
+        <div class="caption-container">
             <img src="/img/users/4.jpg" alt="">
             <div class="caption-container--container grey-overlay">
                 <div class="caption-container--text">
@@ -237,24 +280,74 @@ $this->title = 'Panel de Control';
 
 <!-- Reservaciones -->
 <div class="parallax-container" style="background-color: rgba(41, 103, 155, 0.4);">
-    <div class="container-small">
-        <h1 class="raleway-bold">
-            <div style="background-color:  rgba(61, 155, 233, 1); max-width: 50px; min-height: 15px;" id="clients"
+    <div class="container">
+        <h1 class="raleway-bold white-text">
+            <div style="background-color:  rgb(255, 255, 255); max-width: 50px; min-height: 15px;" id="reservation"
                  class="scrollspy"></div>
-            Clientes
+            Reservaciones
         </h1>
-        <div class="row">
-            <p class="raleway">
-                I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or
-                double click me to add your own content and make changes to the font. Feel free to drag and
-                drop me anywhere you like on your page. I’m a great place for you to tell a story and let your
-                users know a little more about you
-            </p>
+        <div class="row white-text">
+            <div class="col s12 m6 l3">
+                <h2 class="raleway-bold">106</h2>
+                <p class="raleway-bold">CÁMARAS</p>
+            </div>
+            <div class="col s12 m6 l3">
+                <h2 class="raleway-bold">198</h2>
+                <p class="raleway-bold">GRÚAS</p>
+            </div>
+            <div class="col s12 m6 l3">
+                <h2 class="raleway-bold">506</h2>
+                <p class="raleway-bold">MÓVILES</p>
+            </div>
+            <div class="col s12 m6 l3">
+                <h2 class="raleway-bold">1603</h2>
+                <p class="raleway-bold">FILMACIONES</p>
+            </div>
         </div>
     </div>
-    <div class="parallax"><img src="\img\filters.jpg" alt="" style="opacity: 0.7" class="responsive-img"></div>
+    <div class="parallax"><img src="\img\parallax_user.jpg" alt="" style="opacity: 0.7" class="responsive-img"></div>
 </div>
 
+<!-- Contacto -->
+<div class="section-big white scrollspy" id="support">
+    <div class="container-small">
+        <h1 class="raleway-bold">
+            <div style="background-color:  rgba(61, 155, 233, 1); max-width: 50px; min-height: 15px;"></div>
+            Soporte
+        </h1>
+        <div class="row raleway">
+            <div class="col s12 l6">
+                <div class="row">
+                    <div class="col s12">
+                        <input type="text" placeholder="Nombre">
+                    </div>
+                    <div class="col s12">
+                        <input type="text" placeholder="Email">
+                    </div>
+                    <div class="col s12">
+                        <input type="text" placeholder="Asunto">
+                    </div>
+                    <div class="col s12">
+                        <textarea name="message" id="" cols="30" rows="10" class="materialize-textarea" placeholder="Mensaje"></textarea>
+                    </div>
+                    <div class="col s12">
+                        <a href="!#" class="btn waves-effect">Enviar</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 l6">
+                <p class="raleway-bold">Esperamos tus comentarios</p>
+                <p>500 Terry Francois Street,<br>
+                    San Francisco,<br>
+                    CA 94158</p>
+                <p class="raleway-bold">
+                    cttexp_info@wapp.com<br>
+                    123-456-7890
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
 <!-- Table of Contents -->
 <div class="row content-table">
     <div class="col hide-on-small-only table-of-contents">
